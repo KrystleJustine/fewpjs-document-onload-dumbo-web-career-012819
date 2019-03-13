d857be1d2ce44813fe6c009848e817a21a8e3276
Krystle Justine <KrystleJustine@gmail.com>
2019-03-13 15:57:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    swapText()
+});
+
+const swapText = () => {
+    document.getElementById('text').innerHTML = 'This is really cool!';
+}
+
+
